bdfefe9198eb60426851f91bc31fb7b17d81c655
ixaxaar <perceptionvoid@gmail.com>
2015-02-26 03:04:26 +0530

    STDP for oscillation inhibition

--- oscillation_inhibition.py
+++ oscillation_inhibition.py
@@ -12,7 +12,7 @@ import nest.topology as tp
 import uuid
 
 
-def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, ac_amp, ac_freq):
+def run(total, ratio_e, ratio_i, extent, wt_e, wt_i, delay_min, delay_max, ac_amp, ac_freq):
   pop_e = total*ratio_e
   pop_i = total*ratio_i
   ratio = ratio_e/ratio_i
@@ -50,16 +50,16 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
     'edge_wrap': True
   })
 
-  # nest.CopyModel('stdp_synapse', 'excitatory', {'Wmax': 10.0})
-  nest.CopyModel('static_synapse', 'excitatory')
+  nest.CopyModel('stdp_synapse', 'excitatory', {'Wmax': 10.0})
+  # nest.CopyModel('static_synapse', 'excitatory')
   nest.CopyModel('static_synapse', 'inhibitory')
 
   e_i = {
     'connection_type': 'divergent',
     'synapse_model': 'excitatory',
     'weights': {
-      # 'uniform': { 'min': 0.0, 'max': wt_max }
-      'gaussian': {'p_center': 1., 'sigma': 1.}
+      # 'uniform': { 'min': 0.0, 'max': wt_e }
+      'gaussian': {'p_center': wt_e, 'sigma': 1.}
     },
     'delays': {
       'uniform': { 'min': delay_min, 'max': delay_max }
@@ -70,8 +70,8 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
     'connection_type': 'divergent',
     'synapse_model': 'inhibitory',
     'weights': {
-      # 'uniform': { 'min': wt_min*ratio, 'max': 0.0 }
-      'gaussian': {'p_center': -1.*ratio, 'sigma': 1.}
+      # 'uniform': { 'min': wt_i*ratio, 'max': 0.0 }
+      'gaussian': {'p_center': wt_i*ratio, 'sigma': 1.}
     },
     'delays': {
       'uniform': { 'min': delay_min, 'max': delay_max }
@@ -100,7 +100,7 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
   tp.ConnectLayers(ac, e, {'connection_type': 'divergent'})
   tp.ConnectLayers(ac, i, {'connection_type': 'divergent'})
   tp.ConnectLayers(e, detector, {'connection_type': 'divergent'})
-  # tp.ConnectLayers(i, detector, {'connection_type': 'divergent'})
+  tp.ConnectLayers(i, detector, {'connection_type': 'divergent'})
 
 
   nest.Simulate(1000)
@@ -109,20 +109,20 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
   n1id = tuple([x for x in xrange(n1, int(n1+(rad_e*rad_e)))])
   n1c = nest.GetConnections(n1id)
   w1 = nest.GetStatus(n1c, 'weight')
-  # pylab.hist(w1, bins=100)
+  pylab.hist(w1, bins=100)
 
   pylab.figure()
   n2 = i[0] + 1
   n2id = tuple([x for x in xrange(n2, int(n2+(rad_i*rad_i)))])
   n2c = nest.GetConnections(n2id)
   w2 = nest.GetStatus(n2c, 'weight')
-  # pylab.hist(w2, bins=100)
+  pylab.hist(w2, bins=100)
 
   # pylab.figure()
   spike_rows = 1
   spike_id = detector[0]+1
   spike_ids = tuple([x for x in xrange(spike_id, spike_id+(spike_rows*spike_rows))])
-  # nest.raster_plot.from_device(spike_ids, hist=True)
+  nest.raster_plot.from_device(spike_ids, hist=True)
 
   # find how many times it spiked
   sid = nest.GetStatus(spike_ids, 'events')
@@ -130,7 +130,7 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
   h = np.histogram(sid[0]['times'], bins=100)
   h = h[0][h[0] > 0.]
 
-  # pylab.show()
+  pylab.show()
   print "Spiked " + str(len(h)) + " times"
 
   return ({
@@ -139,8 +139,8 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
     'ratio_e': ratio_e,
     'ratio_i': ratio_i,
     'extent': extent,
-    'wt_max': wt_max,
-    'wt_min': wt_min,
+    'wt_e': wt_e,
+    'wt_i': wt_i,
     'delay_min': delay_min,
     'delay_max': delay_max,
     'ac_amp': ac_amp,
@@ -149,64 +149,61 @@ def run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, a
 
 
 
-total = 1000
+total = 2000
 ratio_e = 0.8
 ratio_i = 0.2
 extent = 1.0
-wt_max = .1
-
-# keep increasing this to see awesome bifurcation effect, around 0.8-0.9
-wt_min = -.1
+wt_e = 1.
+wt_i = -1.
 
 delay_min = 0.1
 delay_max = 0.2
 ac_amp = 300.0
-ac_freq = 100.0
+ac_freq = 4.0
 
 results = []
 
-for ac_freq in xrange(2, 50, 2):
-  for ac_amp in xrange(300, 500, 10):
-    r = {}
-    try:
-      r = run(total, ratio_e, ratio_i, extent, wt_max, wt_min, delay_min, delay_max, float(ac_amp), float(ac_freq))
-      results.append(r)
-    except Exception as e:
-      results.append({
-        'spikes': 0,
-        'total': total,
-        'ratio_e': ratio_e,
-        'ratio_i': ratio_i,
-        'extent': extent,
-        'wt_max': wt_max,
-        'wt_min': wt_min,
-        'delay_min': delay_min,
-        'delay_max': delay_max,
-        'ac_amp': ac_amp,
-        'ac_freq': ac_freq
-      })
+# for wt_e in np.arange(0, 10, .1):
+#   for wt_i in np.arange(wt_e/4, wt_e*4, wt_e/4):
+r = {}
+try:
+  r = run(total, ratio_e, ratio_i, extent, wt_e, wt_i, delay_min, delay_max, ac_amp, ac_freq)
+  results.append(r)
+except Exception as e:
+  results.append({
+    'spikes': 0,
+    'total': total,
+    'ratio_e': ratio_e,
+    'ratio_i': ratio_i,
+    'extent': extent,
+    'wt_e': wt_e,
+    'wt_i': wt_i,
+    'delay_min': delay_min,
+    'delay_max': delay_max,
+    'ac_amp': ac_amp,
+    'ac_freq': ac_freq
+  })
 
-    pylab.close('all')
-    print "---------------------------------------------------------------"
-    print r
-    # print "total = " + str(total)
-    # print "ratio_e = " + str(ratio_e)
-    # print "ratio_i = " + str(ratio_i)
-    # print "extent = " + str(extent)
-    # print "wt_max = " + str(wt_max)
-    # print "wt_min = " + str(wt_min)
-    # print "delay_min = " + str(delay_min)
-    # print "delay_max = " + str(delay_max)
-    # print "ac_amp = " + str(ac_amp)
-    # print "ac_freq = " + str(ac_freq)
-    print "---------------------------------------------------------------"
+# pylab.close('all')
+print "---------------------------------------------------------------"
+print r
+# print "total = " + str(total)
+# print "ratio_e = " + str(ratio_e)
+# print "ratio_i = " + str(ratio_i)
+# print "extent = " + str(extent)
+# print "wt_e = " + str(wt_e)
+# print "wt_i = " + str(wt_i)
+# print "delay_min = " + str(delay_min)
+# print "delay_max = " + str(delay_max)
+# print "ac_amp = " + str(ac_amp)
+# print "ac_freq = " + str(ac_freq)
+print "---------------------------------------------------------------"
 
+# wt_e = [a["wt_e"] for a in results]
+# wt_i = [a["wt_i"] for a in results]
+# sps = [a["spikes"] for a in results]
 
-amps = [a[ac_amp] for a in results]
-freqs = [a[ac_freq] for a in results]
-sps = [a[spikes] for a in results]
-
-from mpl_toolkits.mplot3d import Axes3D
-ax =  Axes3D(pylab.figure())
-ax.scatter(freqs, amps, sps)
-pylab.show()
+# from mpl_toolkits.mplot3d import Axes3D
+# ax =  Axes3D(pylab.figure())
+# ax.scatter(wt_e, wt_i, sps)
+# pylab.show()
