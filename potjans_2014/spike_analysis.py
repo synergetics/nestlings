85e5f08327660133861f3efc4b251bbe1a233b40
ixaxaar <perceptionvoid@gmail.com>
2015-02-27 23:53:49 +0530

    Some cleanups

--- potjans_2014/spike_analysis.py
+++ potjans_2014/spike_analysis.py
@@ -1,23 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# spike_analysis.py
-#
-# This file is part of NEST.
-#
-# Copyright (C) 2004 The NEST Initiative
-#
-# NEST is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# NEST is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 # Merges spike files, produces raster plots, calculates and plots firing rates
 
@@ -145,7 +125,7 @@ for i in range(len(tableau20)):
   r, g, b = tableau20[i]
   tableau20[i] = (r / 255., g / 255., b / 255.)
 
-clrs = ['0','0.5','0','0.5','0','0.5','0','0.5']
+clrs = tableau20
 plt.ion()
 
 # raster plot
