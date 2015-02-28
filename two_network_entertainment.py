8446bd1e44142073054b476bd7da690e6d108aa9
ixaxaar <perceptionvoid@gmail.com>
2015-02-28 21:44:44 +0530

    Two network entertained

--- two_network_entertainment.py
+++ two_network_entertainment.py
@@ -8,102 +8,129 @@ import numpy as np
 import pylab
 import nest
 import nest.raster_plot
+import nest.voltage_trace
 import nest.topology as tp
+import ggplot
 
 
 nest.ResetKernel()
 nest.SetKernelStatus({'local_num_threads': 8})
 
-network_radiusulation = 100
-excitatory_ratio = 0.8
+nr_populations = 2
+neuron_model = 'iaf_psc_exp'
 
-I_CDC_e = (10.1, 11.3)
-I_CDC_i = (3.8, 6.3)
+model_params = {'tau_m': 10.,        # membrane time constant (ms)
+                'tau_syn_ex': 0.5,   # excitatory synaptic time constant (ms)
+                'tau_syn_in': 0.5,   # inhibitory synaptic time constant (ms)
+                't_ref': 2.,         # absolute refractory period (ms)
+                'E_L': -65.,         # resting membrane potential (mV)
+                'V_th': -50.,        # spike threshold (mV)
+                'C_m': 250.,         # membrane capacitance (pF)
+                'V_reset': -65.      # reset potential (mV)
+               }
 
-simulation_time = 600
+input_rate = 100.
+input_degrees = []
+input_delays = [1.5, 0.75]
+input_weights = 7.
+frequencies = [11., 27.]
 
-def rounded_radius(pop):
-  return int(math.sqrt(pop))
+populations = np.random.normal(100, 10, nr_populations)
+# for p in populations:
+#   input_degrees.append(np.abs(np.random.normal(p, p/10.)))
 
-inhibitory_ratio = 1 - excitatory_ratio
-excitatory_radius = rounded_radius(network_radiusulation*excitatory_ratio)
-inhibitory_radius = rounded_radius(network_radiusulation*inhibitory_ratio)
+input_degrees = [50., 50.]
 
-e = tp.CreateLayer({
-  'rows': excitatory_radius,
-  'columns': excitatory_radius,
-  'elements': 'iaf_psc_alpha',
-  'extent': [1.0,1.0],
-  'edge_wrap': True
-})
+print input_degrees
 
-i = tp.CreateLayer({
-  'rows': inhibitory_radius,
-  'columns': inhibitory_radius,
-  'elements': 'iaf_psc_alpha',
-  'extent': [1.0,1.0],
-  'edge_wrap': True
-})
+populations = np.array(map(int, populations))
 
-e_i = {
-  'connection_type': 'divergent',
-  'kernel': {
-    'uniform': { 'min': 0.64, 'max': 0.65 }
+neurons = np.zeros_like(populations).tolist()
+inputs = np.zeros_like(populations).tolist()
+noises = np.zeros_like(populations).tolist()
+spikes = np.zeros_like(populations).tolist()
+voltmeters = np.zeros_like(populations).tolist()
+
+nest.SetDefaults(neuron_model, model_params)
+
+# create nodes
+for ctr in xrange(len(populations)):
+  n = nest.Create(neuron_model, populations[ctr])
+  neurons[ctr] = n
+
+  i = nest.Create('sinusoidal_poisson_generator')
+  nest.SetStatus(i, {
+    'dc': input_rate*input_degrees[ctr],
+    'ac': input_rate*input_degrees[ctr],
+    'freq': frequencies[ctr]
+  })
+  inputs[ctr] = i
+
+  p = nest.Create('poisson_generator')
+  nest.SetStatus(p, {
+    'rate': input_rate*input_degrees[ctr]*20.
+  })
+  noises[ctr] = p
+
+  sp = nest.Create('spike_detector')
+  spikes[ctr] = sp
+
+  v = nest.Create('voltmeter')
+  voltmeters[ctr] = v
+
+
+# connect the nodes
+prev_pop = None
+for ctr in xrange(len(populations)):
+  if ctr < (len(populations)-1):
+    prev_pop = neurons[ctr+1]
+  elif len(populations) == 1:
+    prev_pop = neurons[ctr]
+  else:
+    prev_pop = neurons[ctr-1]
+
+  conn_dict = {
+    'rule': 'fixed_total_number',
+    'N': populations[ctr]*100
   }
-}
 
-e_e = {
-  'connection_type': 'divergent',
-  'kernel': {
-    'uniform': { 'min': 0.29, 'max': 0.3 }
+  syn_dict = {
+    # 'delay': {
+    #   'mu': 1.5,
+    #   'distribution': 'normal_clipped',
+    #   'sigma': 0.75,
+    #   'low': 0.1
+    # },
+    'delay': 1.5,
+    'model': 'stdp_synapse',
+    'weight': {
+      'distribution': 'normal_clipped',
+      'mu': 10.,
+      'sigma': 1.,
+      'low': 0.0
+    }
   }
-}
 
-i_e = {
-  'connection_type': 'divergent',
-  'kernel': {
-    'uniform': { 'min': 0.59, 'max': 0.6 }
-  }
-}
+  nest.Connect(neurons[ctr], prev_pop, conn_dict, syn_dict)
 
-i_i = {
-  'connection_type': 'divergent',
-  'kernel': {
-    'uniform': { 'min': 0.054, 'max': 0.055 }
-  }
-}
+  nest.Connect(inputs[ctr], neurons[ctr], 'all_to_all',
+    {'weight': input_weights, 'delay': input_delays[0]})
 
-tp.ConnectLayers(e, i, e_i)
-tp.ConnectLayers(i, e, i_e)
-tp.ConnectLayers(e, e, e_e)
-tp.ConnectLayers(i, i, i_i)
+  nest.Connect(noises[ctr], neurons[ctr], 'all_to_all',
+    {'weight': input_weights, 'delay': input_delays[0]})
 
-nest.CopyModel('dc_generator', 'dc', {'amplitude': 4000.0})
-ac_rows = 1
-ac = tp.CreateLayer({
-  'rows': ac_rows,
-  'columns': ac_rows,
-  'elements': ['dc'],
-  'extent': [1.0, 1.0]
-})
+  nest.Connect(neurons[ctr], spikes[0])
+  nest.Connect(inputs[ctr], spikes[0])
+  # nest.Connect(voltmeters[0], neurons[ctr])
 
-tp.ConnectLayers(ac, e, {'connection_type': 'divergent'})
-tp.ConnectLayers(ac, i, {'connection_type': 'divergent'})
 
-spike_rows = 1
-spikes = tp.CreateLayer({
-  'rows': spike_rows,
-  'columns': spike_rows,
-  'elements': 'spike_detector',
-  'extent': [1.0, 1.0]
-})
-tp.ConnectLayers(e, spikes, {'connection_type': 'divergent'})
-tp.ConnectLayers(i, spikes, {'connection_type': 'divergent'})
+nest.Simulate(1000)
 
-nest.Simulate(simulation_time)
+# for sp in spikes:
+nest.raster_plot.from_device(spikes[0], hist=True)
 
-spike_id = spikes[0]+1
-spike_ids = tuple([x for x in xrange(spike_id, spike_id+(spike_rows*spike_rows))])
-nest.raster_plot.from_device(spike_ids)
+# for v in voltmeters:
+  # pylab.figure()
+# nest.voltage_trace.from_device(voltmeters[0])
+
 pylab.show()
-
